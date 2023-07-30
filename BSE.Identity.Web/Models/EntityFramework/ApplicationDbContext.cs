ff7638e15d7e8ae852f19d81a79f8eb73c89e029
Uwe Eichkorn <git.bsetunes@outlook.com>
2023-07-30 10:34:27 +0200

    resetting ApplicationDbContext

--- BSE.Identity.Web/Models/EntityFramework/ApplicationDbContext.cs
+++ BSE.Identity.Web/Models/EntityFramework/ApplicationDbContext.cs
@@ -6,7 +6,14 @@ namespace BSE.Identity.Web.Models.EntityFramework
 {
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
-		static ApplicationDbContext()
+        /*
+		 * property RefreshTokens is necessary because of preventing the error:
+		 * 
+		 * The model backing the 'ApplicationDbContext' context has changed since the database was created.
+		 * Consider using Code First Migrations to update the database
+		 */
+        DbSet<RefreshToken> RefreshTokens { get; set; }
+        static ApplicationDbContext()
 		{
 			Database.SetInitializer(new MySqlInitializer());
 		}
