ee21134f1b0761fc028f818f9bc839522dbe177a
Uwe Eichkorn <git.bsetunes@outlook.com>
2024-07-24 08:02:40 +0200

    asp.identity project added

--- BSE.Identity.Web/Models/SelectUserRolesViewModel.cs
+++ BSE.Identity.Web/Models/SelectUserRolesViewModel.cs
@@ -34,7 +34,7 @@ namespace BSE.Identity.Web.Models
 			foreach (var userRole in user.Roles)
 			{
 				var checkUserRole =
-					this.Roles.Find(r => r.RoleName == userRole.Role.Name);
+					this.Roles.Find(r => r.RoleName == userRole.RoleId);
 				checkUserRole.Selected = true;
 			}
 		}
